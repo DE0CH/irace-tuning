f7aa4d4b48656b9907895cb8fc514a9d22c38b6c
Deyao Chen <chendeyao000@gmail.com>
2022-12-26 15:20:45 +0800

    remove evaluation script

--- extract-confs.R
+++
@@ -1,7 +0,0 @@
-load("log.Rdata")
-rs <- iraceResults
-nIterations <- length(rs$allElites)
-confs <- rs$allElites[[nIterations]]
-confs <- c(1, confs)
-elites <- rs$allConfigurations[confs,]
-write.csv(elites, "confs.csv", row.names=FALSE)
